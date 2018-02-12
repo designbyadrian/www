05f5b6c4e7793f83d79f62b86e0d1943b38f07bf
Adrian von Gegerfelt <adrian@designbyadrian.com>
2018-02-12 20:16:20 +0000

    👠 what’s needed for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,11 @@ import Link from 'gatsby-link'
 import styles from './footer.module.sass'
 
 const Footer = () => (
-  <div className={styles.footer}>Footer</div>
+  <div className={styles.footer}>
+    <div className={styles.container}>
+      hello thar!
+    </div>
+  </div>
 )
 
 export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,21 +4,26 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import styles from './index.module.sass'
 import './index.sass'
 
 const TemplateWrapper = ({ children }) => (
-  <div className="container">
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <main>
-      {children()}
-    </main>
+  <div className={styles.flexWrapper}>
+    <div className={styles.wrapper}>
+      <Helmet
+        title="Gatsby Default Starter"
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header />
+      <div className={styles.container}>
+        <main>
+          {children()}
+        </main>
+      </div>
+    </div>
     <Footer />
   </div>
 )
