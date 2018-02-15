1c51a68c470a68f0d089b900fadb10c9c881abf6
Adrian von Gegerfelt <adrian@designbyadrian.com>
2018-02-15 16:23:27 +0000

    🐭 minor stuff

--- src/components/KeyFeatures/index.js
+++ src/components/KeyFeatures/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 import styles from './features.module.sass'
 
-const Featyres = ({ children }) => (
+const Features = ({ children }) => (
   <div className={styles.features}>
     <ul>{children}</ul>
   </div>
 )
 
-export default Featyres
+export default Features
