11b9c21ce6d06687d6a82e52f84ad8351faeba36
Adrian von Gegerfelt <adrian@designbyadrian.com>
2018-02-16 12:12:30 +0000

    😂 cute icon animations

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -5,7 +5,7 @@ import styles from './feature.module.sass'
 
 const Feature = props => (
   <div className={styles.feature}>
-    <div className={styles.icon}>
+    <div className={styles.icon} style={{ animationDelay: `${Math.random() * 5000}ms` }}>
       <div>
         <FontAwesomeIcon icon={[props.iconprefix || 'fal', props.icon]} />
         <img className={styles.balloon} src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt=""/>
