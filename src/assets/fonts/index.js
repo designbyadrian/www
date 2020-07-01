bf230056d41fd665c934a78a32c721366f4e1374
Adrian von Gegerfelt <adrian@designbyadrian.com>
2020-07-01 19:33:45 +0200

    😤 Would you believe that everything is case-sensitive?

--- src/assets/fonts/index.js
+++ src/assets/fonts/index.js
@@ -1,6 +1,6 @@
-import Dense_eot from "./Dense.eot"
-import Dense_ttf from "./Dense.ttf"
-import Dense_woff from "./Dense.woff"
-import Dense_woff2 from "./Dense.woff2"
+import Dense_eot from "./dense.eot"
+import Dense_ttf from "./dense.ttf"
+import Dense_woff from "./dense.woff"
+import Dense_woff2 from "./dense.woff2"
 
 export { Dense_eot, Dense_ttf, Dense_woff, Dense_woff2 }
