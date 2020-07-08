04548b442509c56ec718c9d106bebdc2f9502468
Adrian von Gegerfelt <adrian@designbyadrian.com>
2020-07-08 18:39:20 +0200

    🍔 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,103 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "gatsby"
+import tw, { styled } from "twin.macro"
 
 import Layout from "components/Layout"
 import SEO from "components/SEO"
+import Wrapper from "components/PageWrapper"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>That's, like... a bummer...</p>
-  </Layout>
-)
+const sortFunc = (a, b) => b.article?.length || 0 - a.article?.length || 0
+
+const Page = styled(Wrapper)`
+  ${tw`mt-20 text-center dark:text-gray-600`}
+
+  h1 {
+    ${tw`mt-4 mb-0`}
+  }
+
+  h2 {
+    ${tw`mt-20 mb-8 text-gray-600`}
+  }
+
+  p {
+    ${tw`mt-0 text-gray-600`}
+  }
+`
+
+const TagList = tw.ul`list-none`
+
+const Tag = tw(
+  Link
+)`inline-block mb-1 mr-1 px-6 py-2 font-display text-3xl shadow-sm rounded-sm transition-colors duration-300 hover:text-white hover:bg-purple-500 dark:text-gray-200 dark:bg-purple-700`
+
+const NotFoundPage = ({ data, location }) => {
+  const {
+      allContentfulTag: { nodes: tags },
+      allContentfulCategory: { nodes: categories },
+    } = data,
+    { pathname } = location
+
+  tags.sort(sortFunc)
+  categories.sort(sortFunc)
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <Page>
+        <FontAwesomeIcon
+          icon="hamburger"
+          mask="circle"
+          transform="shrink-6"
+          size="10x"
+        />
+        <h1>Bummer</h1>
+        <p>
+          We're currently out of <strong>“{pathname}”</strong>!<br />
+          Can we interest you in something else from the menu?
+        </p>
+
+        <h2>How about something from these categories?</h2>
+
+        <TagList>
+          {categories.map(category => (
+            <Tag to={`/${category.slug}`}>{category.title}</Tag>
+          ))}
+        </TagList>
+
+        <h2>Or maybe something with one of the following ingredients?</h2>
+
+        <TagList>
+          {tags.map(tag => (
+            <Tag to={`/${tag.slug}`}>{tag.title}</Tag>
+          ))}
+        </TagList>
+      </Page>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulTag {
+      nodes {
+        slug
+        article {
+          id
+        }
+        title
+      }
+    }
+    allContentfulCategory {
+      nodes {
+        title
+        slug
+        article {
+          id
+        }
+      }
+    }
+  }
+`
 
 export default NotFoundPage
--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,5 +1,6 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
 
+import { faHamburger } from "@fortawesome/free-solid-svg-icons/faHamburger"
 import { faCircle } from "@fortawesome/free-solid-svg-icons/faCircle"
 import { faCut } from "@fortawesome/free-solid-svg-icons/faCut"
 import { faCocktail } from "@fortawesome/free-solid-svg-icons/faCocktail"
@@ -7,4 +8,12 @@ import { faHandSpock } from "@fortawesome/free-solid-svg-icons/faHandSpock"
 import { faQuestion } from "@fortawesome/free-solid-svg-icons/faQuestion"
 import { faSave } from "@fortawesome/free-solid-svg-icons/faSave"
 
-library.add(faCut, faCircle, faCocktail, faHandSpock, faQuestion, faSave)
+library.add(
+  faHamburger,
+  faCut,
+  faCircle,
+  faCocktail,
+  faHandSpock,
+  faQuestion,
+  faSave
+)
