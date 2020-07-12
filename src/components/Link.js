ba6baac44d11fbf75f1d4ca6c12bd219e80c6c1a
Adrian von Gegerfelt <adrian@designbyadrian.com>
2020-07-12 17:05:11 +0200

    🎨 Simpler link styles

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,29 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 import tw, { styled } from "twin.macro"
-import { Link as GatsbyLink } from "gatsby"
+import { css } from "@emotion/core"
+import { Link } from "gatsby"
 
-const StyledAnchor = styled.a`
+const linkStyle = css`
   ${tw`relative`}
 
   &:not(.gatsby-resp-image-link)::after {
     content: "";
-    ${tw`absolute left-0 right-0 border-b border-gray-300 dark:border-gray-600 transition-colors duration-200`}
-    bottom: -0.25em;
-  }
-
-  &:hover::after {
-    ${tw`border-purple-600 dark:border-purple-500`}
-  }
-`
-
-const StyledLink = styled(GatsbyLink)`
-  ${tw`relative`}
-
-  &::after {
-    content: "";
-    ${tw`absolute left-0 right-0 border-b border-gray-300 dark:border-gray-600 transition-colors duration-200`}
-    bottom: -0.25em;
+    ${tw`absolute left-0 right-0 border-b border-gray-400 dark:border-gray-600 transition-colors duration-200`}
+    bottom: -0.2em;
   }
 
   &:hover::after {
@@ -33,9 +20,9 @@ const StyledLink = styled(GatsbyLink)`
 
 const Anchor = props =>
   props.href ? (
-    <StyledAnchor {...props} target="_blank" rel="noopener noreferrer" />
+    <a css={linkStyle} {...props} target="_blank" rel="noopener noreferrer" />
   ) : (
-    <StyledLink {...props} />
+    <Link css={linkStyle} {...props} />
   )
 
 export default Anchor
